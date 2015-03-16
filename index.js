0c5f1a26c680a4d9b59abd4c6872d8398a9b27ae
David Aurelio <david@uxebu.com>
2015-03-16 16:41:10 +0100

    Complete initial implementation

--- index.js
+++ index.js
@@ -1,84 +1,98 @@
 'use strict';
 
-var astTypes = require('ast-types');
+var builders = require('ast-types').builders;
 var replace = require('estraverse').replace;
 
-var namedTypes = astTypes.namedTypes;
-var CallExpression = namedTypes.CallExpression;
-var ExpressionStatement = namedTypes.ExpressionStatement;
-var FunctionExpression = namedTypes.FunctionExpression;
-var Identifier = namedTypes.Identifier;
-var MemberExpression = namedTypes.MemberExpression;
+var arrayExpression = builders.arrayExpression;
+var blockStatement = builders.blockStatement;
+var callExpression = builders.callExpression;
+var expressionStatement = builders.expressionStatement;
+var identifier = builders.identifier;
+var functionExpression = builders.functionExpression;
+var literal = builders.literal;
+var memberExpression = builders.memberExpression;
+var variableDeclaration = builders.variableDeclaration;
+var variableDeclarator = builders.variableDeclarator;
 
-var arrayExpression = astTypes.builders.arrayExpression;
-var identifier = astTypes.builders.identifier;
-var variableDeclaration = astTypes.builders.variableDeclaration;
-var variableDeclarator = astTypes.builders.variableDeclarator;
+function isFunction(node) {
+  return node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression';
+}
+
+function isStringLiteral(node) {
+  return node.type === 'Literal' && typeof node.value === 'string';
+}
+
+function isPropertyAccess(node) {
+  return node.type === 'MemberExpression' && !node.computed;
+}
+
+function isPropertyKey(node, parent) {
+  return parent.type === 'Property' && parent.key === node;
+}
 
 exports.transformAst = function(ast, createVariableName) {
   var usedVariables = {};
+  var exposesVariables = false;
+  var strings = [];
+  var stringIndexes = Object.create(null);
+  var stringMapIdentifier = identifier('');
+
+  function addString(string) {
+    if (!(string in stringIndexes)) {
+      stringIndexes[string] = strings.push(string) - 1;
+    }
+    return stringIndexes[string];
+  }
 
   replace(ast, {
-    enter: function(node) {
-      if (Identifier.check(node)) {
+    enter: function(node, parent) {
+      var index;
+      if (node.type === 'VariableDeclaration') {
+        if (!exposesVariables) {
+          exposesVariables = !this.parents().some(isFunction);
+        }
+      } else if (node.type === 'Identifier') {
         usedVariables[node.name] = true;
+      } else if (isStringLiteral(node) && !isPropertyKey(node, parent)) {
+        index = addString(node.value);
+        return memberExpression(stringMapIdentifier, literal(index), true);
+      } else if (isPropertyAccess(node)) {
+        index = addString(node.property.name);
+        return memberExpression(node.object,
+          memberExpression(stringMapIdentifier, literal(index), true), true);
       }
     }
   });
 
-  var variableName = createVariableName(Object.keys(usedVariables));
-  var place = getPlacement(ast.body);
-  place(identifier(variableName), arrayExpression([]));
+  stringMapIdentifier.name = createVariableName(Object.keys(usedVariables));
+  var insertMap = exposesVariables ? prependMap : wrapWithIife;
+  ast.body = insertMap(ast.body, stringMapIdentifier, arrayExpression(strings.map(literal)));
 
   return ast;
 };
 
-function getPlacement(body) {
-  var isSingleStatement = body.length === 1;
-  var firstStatement = body[0];
-  if (isSingleStatement && isIife(firstStatement)) {
-    return function(identifier, arrayExpression) {
-      var call = firstStatement.expression, callee = call.callee;
-      callee.params.push(identifier);
-      call.arguments.push(arrayExpression);
-    };
-  }
-
-  if (isSingleStatement && isIifeWithCall(firstStatement)) {
-    return function(identifier, arrayExpression) {
-      var call = firstStatement.expression, callee = call.callee.object;
-      callee.params.push(identifier);
-      call.arguments.push(arrayExpression);
-    };
-  }
-
-  return function(identifier, arrayExpression) {
-    body.unshift(
-      variableDeclaration('var', [
-        variableDeclarator(identifier, arrayExpression)
-      ])
-    );
-  };
+function wrapWithIife(body, stringMapName, stringMap) {
+  var wrapperFunctionBody = blockStatement(body);
+  var wrapperFunction = functionExpression(null, [stringMapName], wrapperFunctionBody);
+  var iife = expressionStatement(
+    callExpression(
+      memberExpression(wrapperFunction, identifier('call'), false),
+      [identifier('this'), stringMap]));
+  return [iife];
 }
 
-function isCallExpressionStatement(node) {
-  return ExpressionStatement.check(node) &&
-         CallExpression.check(node.expression);
+function prependMap(body, stringMapName, stringMap) {
+  return [
+    variableDeclaration('var', [
+      variableDeclarator(stringMapName, stringMap)
+    ])
+  ].concat(body);
 }
 
-function isIife(node) {
-  return isCallExpressionStatement(node) &&
-         FunctionExpression.check(node.expression.callee);
-}
-
-function isIifeInvokedWithCall(callExpression) {
-  return MemberExpression.check(callExpression.callee) &&
-         FunctionExpression.check(callExpression.callee.object) &&
-         Identifier.check(callExpression.callee.property) &&
-         callExpression.callee.property.name === 'call';
-}
-
-function isIifeWithCall(node) {
-  return isCallExpressionStatement(node) &&
-         isIifeInvokedWithCall(node.expression);
-}
+exports.createVariableName = function(variableNames) {
+  var name = '_x';
+  do {
+    name += (Math.random() * 0xffff) | 0;
+  } while (variableNames.indexOf(name) !== -1);
+  return name;
+};
--- test.coffee
+++ test.coffee
@@ -1,4 +1,4 @@
-{transformAst} = require './index'
+{transformAst, createVariableName} = require './index'
 {parse, print} = require 'recast'
 {spy} = require 'sinon'
 assert = require 'assert'
@@ -10,6 +10,9 @@ describe 'AST transformation:', ->
   # Needs to be on the test object, since mocha-given tries to eval().call(this)
   # when tests fail. `toSource` is not in scope in these cases
   Given -> @toSource = toSource
+
+  Given -> @varName = 'arbitrary'
+  Given -> @createName = => @varName
 
   describe 'passes an array of all encountered variable names to the `createName` callback:', ->
     Given -> @ast = toAst('''
@@ -37,28 +40,71 @@ describe 'AST transformation:', ->
     ]
 
   describe 'placement of the string array:', ->
-    Given -> @varName = 'arbitrary'
-    Given -> @createName = => @varName
 
-    describe 'uses the return value of `createName` to place an array at the begin:', ->
-      Given -> @source = '1 + 1;'
-      When  -> @obfuscated = transformAst(toAst(@source), @createName)
-      Then  -> @toSource(@obfuscated) == "var #{@varName} = [];\n#{@source}"
-
-    describe 'places variable name and array as parameter and arguments if the ast represents an IIFE:', ->
-      Given -> @source = '(function() { 1 + 1; }());'
-      Given -> @expected = "((function(#{@varName}) { 1 + 1; })([]));"
+    describe 'wraps code in an IIFE, using the map variable name provided by `createName` and the array as parameter and arguments:', ->
+      Given -> @source = '1 + 1'
+      Given -> @expected = """
+        (function(#{@varName}) {
+          #{@source};
+        }).call(this, [])
+        """
       When  -> @obfuscated = transformAst(toAst(@source), @createName)
       Then  -> @toSource(@obfuscated) == @expected
 
-    describe 'appends to parameter and arguments lists of the IIFE:', ->
-      Given -> @source = '(function(win) { alert(1 + 1); }(window));'
-      Given -> @expected = "((function(win, #{@varName}) { alert(1 + 1); })(window, []));"
+    describe 'Places the string map at the begin (no IIFE) if any variable declarations exist at the top level:', ->
+      Given -> @source = 'while (true) { var foo = 1; }';
+      Given -> @expectedStart = "var #{@varName} = [];"
       When  -> @obfuscated = transformAst(toAst(@source), @createName)
-      Then  -> @toSource(@obfuscated) == @expected
+      Then  -> @toSource(@obfuscated).slice(0, @expectedStart.length) == @expectedStart
 
-    describe 'works with variants where the IIFE is invoked with `call()`', ->
-      Given -> @source = '(function() { this.alert(1 + 1); }.call(this));'
-      Given -> @expected = "((function(#{@varName}) { this.alert(1 + 1); }).call(this, []));"
-      When  -> @obfuscated = transformAst(toAst(@source), @createName)
-      Then  -> @toSource(@obfuscated) == @expected
+  describe 'replacement of strings:', ->
+    Given -> @source = '''call(['a string', true, false, null, 1.2, -3, 'another string']);'''
+    Given -> @expected = """
+      (function(#{@varName}) {
+        call([#{@varName}[0], true, false, null, 1.2, -3, #{@varName}[1]]);
+      }).call(this, ["a string", "another string"]);
+      """
+    When  -> @obfuscated = transformAst(toAst(@source), @createName)
+    Then  -> @toSource(@obfuscated) == @expected
+
+  describe 'replacement of property accesses:', ->
+    Given -> @source = 'object.method(some.value, another.property);';
+    Given -> @expected = """
+      (function(#{@varName}) {
+        object[#{@varName}[0]](some[#{@varName}[1]], another[#{@varName}[2]]);
+      }).call(this, ["method", "value", "property"]);
+      """
+    When  -> @obfuscated = transformAst(toAst(@source), @createName)
+    Then  -> @toSource(@obfuscated) == @expected
+
+  describe 'leaves string properties in object literals intact:', ->
+    Given -> @source = 'call({"a string": 123});';
+    Given -> @expected = """
+      (function(#{@varName}) {
+        #{@source}
+      }).call(this, []);
+      """
+    When  -> @obfuscated = transformAst(toAst(@source), @createName)
+    Then  -> @toSource(@obfuscated) == @expected
+
+  describe 'deduplication of encountered strings:', ->
+    Given -> @source = 'object.arbitrary = "arbitrary" + util.arbitrary("arbitrary")';
+    Given -> @expected = """
+      (function(#{@varName}) {
+        object[#{@varName}[0]] = #{@varName}[0] + util[#{@varName}[0]](#{@varName}[0]);
+      }).call(this, ["arbitrary"])
+      """
+    When  -> @obfuscated = transformAst(toAst(@source), @createName)
+    Then  -> @toSource(@obfuscated) == @expected
+
+
+describe 'variable name creator:', ->
+  describe 'it creates a variable name that maches /^_x\\d+$/:', ->
+    Given -> @varNames = [];
+    When  -> @created = createVariableName(@varNames)
+    Then  -> /_x\d+$/.test(@created)
+
+  describe 'it does not return a passed-in variable name:', ->
+    Given -> @varNames = ["_x#{i}" for i in [0..0xffff]]
+    When  -> @created = createVariableName(@varNames)
+    Then  -> @varNames.indexOf(@created) == -1
